c288a194db75423c1bd7fbb2c09153d3e2def7d1
zxjte9411 <a5852241@gmail.com>
2019-08-05 01:01:06 +0800

    modify settings

--- back_end/back_end/settings/base.py
+++ back_end/back_end/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'stock',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
