7593b5b8ee0e5ffded5506bb7bdfc4e2fed0a31d
zxjte9411 <a5852241@gmail.com>
2019-08-05 21:43:23 +0800

    修正設定上的錯誤

--- back_end/back_end/settings/base.py
+++ back_end/back_end/settings/base.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'stock',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,7 +41,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'stock',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
