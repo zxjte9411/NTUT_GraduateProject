8074de2e2935c5ff3b7771d259723e9e6d08a35f
zxjte9411 <a5852241@gmail.com>
2019-08-05 21:47:37 +0800

    顯示簡單表單及回傳結果

--- back_end/stock/views.py
+++ back_end/stock/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+
+from .TechnologyPointer import TechnologyPointer, stock_nums
+import pandas as pd
+
+def home(request):
+    if request.method == 'POST':
+        form = dict(request.POST)
+        technology_pointer = TechnologyPointer(
+            stock_number=form['stock_num'][0])
+
+        data = {
+            'DMI': f"{round(technology_pointer.get_DMI_profit(int(form['money'][0]))*100, 2)}%",
+            'PSY': f"{round(technology_pointer.get_PSY_profit(int(form['money'][0]))*100, 2)}%",
+            'OBV': f"{round(technology_pointer.get_OBV_profit(int(form['money'][0]))*100, 2)}%",
+            'AR': f"{round(technology_pointer.get_AR_profit(int(form['money'][0]))*100, 2)}%",
+            'BR': f"{round(technology_pointer.get_BR_profit(int(form['money'][0]))*100, 2)}%",
+            'KD': f"{round(technology_pointer.get_KD_profit(int(form['money'][0]))*100, 2)}%",
+            'RSI': f"{round(technology_pointer.get_RSI_profit(int(form['money'][0]))*100, 2)}%",
+            'MA': f"{round(technology_pointer.get_MA_profit(int(form['money'][0]))*100, 2)}%"
+        }
+
+        return render(request, 'resulr.html', {'stock_nums': stock_nums, 'data': data})
+    return render(request, 'home.html', {'stock_nums': stock_nums})
+
