90c7631820d0fe5f5299110ee21630689686048b
zxjte9411 <a5852241@gmail.com>
2019-08-05 21:44:08 +0800

    設定 router

---
+++ back_end/stock/urls.py
@@ -0,0 +1,8 @@
+# from django.conf.urls import include, url
+from django.urls import path
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    path("", views.home, name='home'),
+]
