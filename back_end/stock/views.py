ea5fd61ea9c0823946f1daa60f8f07f588b5c380
zxjte9411 <a5852241@gmail.com>
2019-08-11 22:04:49 +0800

    顯示個指標獲利率

--- back_end/stock/views.py
+++ back_end/stock/views.py
@@ -1,45 +1,61 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 
 from django.shortcuts import render
 
 from .TechnologyPointer import TechnologyPointer, stock_nums
 import pandas as pd
+import json
+
 
 def contact(request):
     return render(request, 'contact.html')
 
+
 def trading(request):
     return render(request, 'trading.html')
+
 
 def withdraw(request):
     if request.method == 'POST':
         form = dict(request.POST)
-        print(form['stock_num'][0])
-        technology_pointer = TechnologyPointer('2019-04-12', form['stock_num'][0])
+        technology_pointer = TechnologyPointer(
+            '2019-04-12', form['stock_num'][0])
 
         data = {
-            'DMI': f"{round(technology_pointer.get_DMI_profit(int(form['money'][0]))*100, 2)}%",
-            'PSY': f"{round(technology_pointer.get_PSY_profit(int(form['money'][0]))*100, 2)}%",
-            'OBV': f"{round(technology_pointer.get_OBV_profit(int(form['money'][0]))*100, 2)}%",
-            'AR': f"{round(technology_pointer.get_AR_profit(int(form['money'][0]))*100, 2)}%",
-            'BR': f"{round(technology_pointer.get_BR_profit(int(form['money'][0]))*100, 2)}%",
-            'KD': f"{round(technology_pointer.get_KD_profit(int(form['money'][0]))*100, 2)}%",
-            'RSI': f"{round(technology_pointer.get_RSI_profit(int(form['money'][0]))*100, 2)}%",
-            'MA': f"{round(technology_pointer.get_MA_profit(int(form['money'][0]))*100, 2)}%"
+            'status': 1,
+            'pointers': [
+                {'pointer': 'DMI',
+                    'value': f"{round(technology_pointer.get_DMI_profit(int(form['money'][0]))*100, 2)}%"},
+                {'pointer': 'PSY',
+                    'value': f"{round(technology_pointer.get_PSY_profit(int(form['money'][0]))*100, 2)}%"},
+                {'pointer': 'OBV',
+                    'value': f"{round(technology_pointer.get_OBV_profit(int(form['money'][0]))*100, 2)}%"},
+                {'pointer': 'AR',
+                    'value': f"{round(technology_pointer.get_AR_profit(int(form['money'][0]))*100, 2)}%"},
+                {'pointer': 'BR',
+                    'value': f"{round(technology_pointer.get_BR_profit(int(form['money'][0]))*100, 2)}%"},
+                {'pointer': 'KD',
+                    'value': f"{round(technology_pointer.get_KD_profit(int(form['money'][0]))*100, 2)}%"},
+                {'pointer': 'RSI',
+                    'value': f"{round(technology_pointer.get_RSI_profit(int(form['money'][0]))*100, 2)}%"},
+                {'pointer': 'MA', 'value': f"{round(technology_pointer.get_MA_profit(int(form['money'][0]))*100, 2)}%"}]
         }
-        return render(request, 'resulr.html', {'data': data})
+        return HttpResponse(json.dumps(data))
+        # return render(request, 'resulr.html', {'data': data})
     return render(request, 'withdraw.html', {'stock_nums': stock_nums})
 
 
 def stockPoint(request):
     return render(request, 'stockPoint.html')
 
+
 def home(request):
     if request.method == 'POST':
         form = dict(request.POST)
-        technology_pointer = TechnologyPointer(stock_number=form['stock_num'][0])
+        technology_pointer = TechnologyPointer(
+            stock_number=form['stock_num'][0])
 
         data = {
             'DMI': f"{round(technology_pointer.get_DMI_profit(int(form['money'][0]))*100, 2)}%",
@@ -53,5 +69,4 @@ def home(request):
         }
 
         return render(request, 'resulr.html', {'data': data})
-    return render(request, 'home.html', {'stock_nums': stock_nums})
-
+    return render(request, 'index.html', {'stock_nums': stock_nums})
