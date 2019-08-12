e93136f8e80e981698661743a4a266d22f61861d
zxjte9411 <a5852241@gmail.com>
2019-08-12 20:49:35 +0800

    刪除多餘 code

--- back_end/stock/views.py
+++ back_end/stock/views.py
@@ -49,24 +49,3 @@ def withdraw(request):
 
 def stockPoint(request):
     return render(request, 'stockPoint.html')
-
-
-def home(request):
-    if request.method == 'POST':
-        form = dict(request.POST)
-        technology_pointer = TechnologyPointer(
-            stock_number=form['stock_num'][0])
-
-        data = {
-            'DMI': f"{round(technology_pointer.get_DMI_profit(int(form['money'][0]))*100, 2)}%",
-            'PSY': f"{round(technology_pointer.get_PSY_profit(int(form['money'][0]))*100, 2)}%",
-            'OBV': f"{round(technology_pointer.get_OBV_profit(int(form['money'][0]))*100, 2)}%",
-            'AR': f"{round(technology_pointer.get_AR_profit(int(form['money'][0]))*100, 2)}%",
-            'BR': f"{round(technology_pointer.get_BR_profit(int(form['money'][0]))*100, 2)}%",
-            'KD': f"{round(technology_pointer.get_KD_profit(int(form['money'][0]))*100, 2)}%",
-            'RSI': f"{round(technology_pointer.get_RSI_profit(int(form['money'][0]))*100, 2)}%",
-            'MA': f"{round(technology_pointer.get_MA_profit(int(form['money'][0]))*100, 2)}%"
-        }
-
-        return render(request, 'resulr.html', {'data': data})
-    return render(request, 'index.html', {'stock_nums': stock_nums})
