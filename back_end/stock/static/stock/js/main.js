0bfecca90c7d018af9c18a7aa4557f22f0f64e78
zxjte9411 <a5852241@gmail.com>
2019-08-19 02:16:57 +0800

    為詳細資訊頁新增/修改邏輯

--- back_end/stock/static/stock/js/main.js
+++ back_end/stock/static/stock/js/main.js
@@ -1,7 +1,7 @@
 $(() => {
-    function update_view() {
+    function update_view_of_detail(action_url) {
         $.ajaxSetup({
-            url: "/withdraw",
+            url: action_url,
             type: 'POST',
             global: false,
             dataType: 'json',
@@ -10,36 +10,80 @@ $(() => {
         $.ajax({
             data: {
                 csrfmiddlewaretoken: $("*[name='csrfmiddlewaretoken']").val(),
-                money: $("#select_2").val(),
-                stock_num: $("#select_1").val()
+                money: $("#money").val(),
+                stock_num: $("#stock_num").val()
             },
-
             success: function (data) {
+                let tptype = $("#tptype").val()
                 if (data.status == 1) {
                     result_table = $("#dataTable")
-                    result_table.html("")
-                    let thead = "<thead><tr><th>指標</th><th>獲利率</th></tr></thead>"
-                    let tbody = $("<tbody></tbody>")
-                    $.each(data.pointers, function (_index, element) {
-                        // console.log(element.pointer)
-                        // console.log(element.value)
-                        tbody.append("<tr><th>" + element.pointer + "</th><th>"+element.value+"</th></tr>")
+                    let tbody
+                    if ($("#dataTable tbody").length === 0) {
+                        tbody = $("<tbody></tbody>")
+                    }
+                    else {
+                        tbody = $("#dataTable tbody")
+                    }
+                    tbody.html("")
+                    data.details[tptype].forEach(element => {
+                        tbody.append("<tr><th>" + element.date + "</th><th>" + element.close + "</th><th>" + element.type + "</tr>")
                     })
-                    result_table.append(thead)
                     result_table.append(tbody)
                     // 第一次發 POST 會改變 table 的顯示
                     $("#result").show()
                 }
             },
-
             error: function (XMLHttpRequest, _ajaxOptions, _errorThrown) {
                 console.log(XMLHttpRequest.status)
-                // console.log(errorThrown)
+            }
+
+        })
+    }
+    function update_view_of_withdraw(action_url) {
+        $.ajaxSetup({
+            url: action_url,
+            type: 'POST',
+            global: false,
+            dataType: 'json',
+        })
+
+        $.ajax({
+            data: {
+                csrfmiddlewaretoken: $("*[name='csrfmiddlewaretoken']").val(),
+                money: $("#money").val(),
+                stock_num: $("#stock_num").val()
+            },
+            success: function (data) {
+                if (data.status == 1) {
+                    result_table = $("#dataTable")
+                    let tbody
+                    if ($("#dataTable tbody").length === 0) {
+                        tbody = $("<tbody></tbody>")
+                    }
+                    else {
+                        tbody = $("#dataTable tbody")
+                    }
+                    tbody.html("")
+                    $.each(data.pointers, function (_index, element) {
+                        tbody.append("<tr><th>" + element.pointer + "</th><th>" + element.value + "</th></tr>")
+                    })
+                    result_table.append(tbody)
+                    // 第一次發 POST 會改變 table 的顯示
+                    $("#result").show()
+                    $("#return_money").val($("#money").val())
+                    $("#return_stock_num").val($("#stock_num").val())
+                }
+            },
+            error: function (XMLHttpRequest, _ajaxOptions, _errorThrown) {
+                console.log(XMLHttpRequest.status)
             }
         })
     }
 
-    $("#button_submit").click(function () {
-        update_view()
+    $("#button_submit_withdraw").click(function () {
+        update_view_of_withdraw("/withdraw")
+    })
+    $("#button_submit_detail").click(function () {
+        update_view_of_detail("/withdraw/detail")
     })
 })
